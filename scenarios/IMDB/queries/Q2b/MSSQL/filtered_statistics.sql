c0895d76a7ee556150c50db1f6d585833928dfdd
tsamoura <tsamoura@cs.ox.ac.uk>
2016-08-04 14:28:19 +0100

    Committing TPCH-MANUAL

--- scenarios/IMDB/queries/Q2b/MSSQL/filtered_statistics.sql
+++ scenarios/IMDB/queries/Q2b/MSSQL/filtered_statistics.sql
@@ -5,6 +5,3 @@ WHERE keyword ='character-name-in-title';
 CREATE STATISTICS company_name_id
     ON company_name(id) 
 WHERE country_code ='[nl]';
-	 
-DROP STATISTICS keyword.keyword_id;
-DROP STATISTICS company_name.company_name_id;
